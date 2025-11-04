edc88c368ede40ba2fe4c22ffa739b5003fb0c49
tj-10 <1010junpei@gmail.com>
2025-11-05 08:42:38 +0900

    Add Rust Hello World project

---
+++ target/debug/deps/hello-18c76508be63545d.d
@@ -0,0 +1,5 @@
+/home/tj-10/03.Projects/03.TIL/01.rust/hello/target/debug/deps/hello-18c76508be63545d.d: src/main.rs
+
+/home/tj-10/03.Projects/03.TIL/01.rust/hello/target/debug/deps/hello-18c76508be63545d: src/main.rs
+
+src/main.rs:
---
+++ target/debug/hello.d
@@ -0,0 +1,1 @@
+/home/tj-10/03.Projects/03.TIL/01.rust/hello/target/debug/hello: /home/tj-10/03.Projects/03.TIL/01.rust/hello/src/main.rs
