a23a02624264fe5176a0e6ac162ec4a1eae9534c
tj-10 <1010junpei@gmail.com>
2025-11-05 09:40:10 +0900

    Remove Rust-related files from 00.github

--- target/debug/deps/hello-18c76508be63545d.d
+++
@@ -1,5 +0,0 @@
-/home/tj-10/03.Projects/03.TIL/01.rust/hello/target/debug/deps/hello-18c76508be63545d.d: src/main.rs
-
-/home/tj-10/03.Projects/03.TIL/01.rust/hello/target/debug/deps/hello-18c76508be63545d: src/main.rs
-
-src/main.rs:
--- target/debug/hello.d
+++
@@ -1,1 +0,0 @@
-/home/tj-10/03.Projects/03.TIL/01.rust/hello/target/debug/hello: /home/tj-10/03.Projects/03.TIL/01.rust/hello/src/main.rs
