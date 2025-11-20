49d38048aae5a74b7966572cfe59013735d97ba5
tj-10 <1010junpei@gmail.com>
2025-11-20 10:00:23 +0900

    auto: 2025-11-20 10:00:22

---
+++ 01.Programming/05.sql/15.view_trigger.sql
@@ -0,0 +1,26 @@
+CREATE TABLE mushroom_log (
+  log_id INTEGER PRIMARY KEY AUTOINCREMENT,
+  mushroom_id INTEGER,
+  action TEXT,
+  timestamp DATETIME DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TRIGGER log_edible_change
+AFTER UPDATE OF edible ON mushrooms
+FOR EACH ROW
+WHEN OLD.edible != NEW.edible
+BEGIN
+  INSERT INTO mushroom_log (mushroom_id, action)
+  VALUES (NEW.id, 'edible changed');
+END;
+
+CREATE VIEW edible_change_view AS
+SELECT
+  l.log_id,
+  m.name,
+  l.action,
+  l.timestamp
+FROM
+  mushroom_log l
+JOIN
+  mushrooms m ON l.mushroom_id = m.id;
