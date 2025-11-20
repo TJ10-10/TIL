2eb945e0a0be615e1159a3f19b54b09065033fc7
tj-10 <1010junpei@gmail.com>
2025-11-20 10:10:17 +0900

    auto: 2025-11-20 10:10:17

---
+++ 01.Programming/06.html_css/15.scroll_triggered_animation/script.js
@@ -0,0 +1,12 @@
+const sections = document.querySelectorAll('.fade-in-section');
+
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add('visible');
+    }
+  });
+});
+
+sections.forEach(section => observer.observe(section));
+
