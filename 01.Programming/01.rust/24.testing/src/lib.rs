dbeb3662bd631747578935173ff31f581776a78b
tj-10 <1010junpei@gmail.com>
2025-11-22 08:13:08 +0900

    Rust test

--- 01.Programming/01.rust/24.testing/src/lib.rs
+++ 01.Programming/01.rust/24.testing/src/lib.rs
@@ -1,5 +1,9 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+pub fn add(a: i32, b: i32) -> i32 {
+    a + b
+}
+
+pub fn divide(a: i32, b: i32) -> Option<i32> {
+    if b == 0 { None } else { Some(a / b) }
 }
 
 #[cfg(test)]
@@ -7,8 +11,17 @@ mod tests {
     use super::*;
 
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    fn test_add() {
+        assert_eq!(add(2, 3), 5);
+    }
+
+    #[test]
+    fn test_divide_ok() {
+        assert_eq!(divide(10, 2), Some(5));
+    }
+
+    #[test]
+    fn test_divide_by_zero() {
+        assert_eq!(divide(10, 0), None);
     }
 }
