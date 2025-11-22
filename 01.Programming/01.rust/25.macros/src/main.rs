9aab7c11179c2b68ae35515727c25b02fcf629e1
tj-10 <1010junpei@gmail.com>
2025-11-23 05:32:31 +0900

    Rust macro

--- 01.Programming/01.rust/25.macros/src/main.rs
+++ 01.Programming/01.rust/25.macros/src/main.rs
@@ -1,3 +1,25 @@
+macro_rules! say_hello {
+    () => {
+        println!("こんにちは、Rustの森へようこそ!");
+    };
+}
+
+macro_rules! print_sum {
+    ($a:expr, $b:expr) => {
+        println!("{} + {} = {}", $a, $b, $a + $b);
+    };
+}
+
+macro_rules! log_values {
+    ($($val:expr),*) => {
+        $(
+            println!("値: {}", $val);
+        )*
+    };
+}
+
 fn main() {
-    println!("Hello, world!");
+    say_hello!();
+    print_sum!(3, 7);
+    log_values!(10, 20, 30, 40);
 }
