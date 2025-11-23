bdc4399f4bc5c05d8b8db551d8dba6be16a7fa4e
tj-10 <1010junpei@gmail.com>
2025-11-23 11:35:45 +0900

    C++ trie

---
+++ 01.Programming/07.data_algo_c++/09.trie/main.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <unordered_map>
+using namespace std;
+
+class TrieNode {
+  public:
+    bool isEnd = false;
+    unordered_map<char, TrieNode*> children;
+};
+
+class Trie {
+  TrieNode* root;
+
+public:
+  Trie() { root = new TrieNode(); }
+
+  void insert(const string& word) {
+    TrieNode* node = root;
+    for (char c : word) {
+      if (!node->children[c])
+        node->children[c] = new TrieNode();
+      node = node->children[c];
+    }
+    node->isEnd = true;
+  }
+
+  bool search(const string& word) {
+    TrieNode* node = root;
+    for (char c : word) {
+      if (!node->children[c]) return false;
+      node = node->children[c];
+    }
+    return node->isEnd;
+  }
+};
+
+int main() {
+  Trie trie;
+  trie.insert("nameko");
+  trie.insert("shiitake");
+  cout << boolalpha << trie.search("nameko") << endl;
+  cout << boolalpha << trie.search("maitake") << endl;
+}
