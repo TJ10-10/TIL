5660c7125f6209b1f5b4ced9cc3faeed6b01bcc4
tj-10 <1010junpei@gmail.com>
2025-12-01 09:47:45 +0900

    Rust serde json

--- 01.Programming/01.rust/27.serde_json/src/main.rs
+++ 01.Programming/01.rust/27.serde_json/src/main.rs
@@ -1,3 +1,24 @@
+use serde::{Deserialize, Serialize};
+use serde_json;
+
+#[derive(Serialize, Deserialize, Debug)]
+struct Mushroom {
+    name: String,
+    color: String,
+    is_edible: bool,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let erin = Mushroom {
+        name: "Erin".to_string(),
+        color: "Golden Orange".to_string(),
+        is_edible: false,
+    };
+    
+    let json = serde_json::to_string(&erin).unwrap();
+    println!("JSONに変換されたErin: {}", json);
+
+    let data = r#"{"name":"Shiitake", "color":"Brown","is_edible":true}"#;
+    let shiitake: Mushroom = serde_json::from_str(data).unwrap();
+    println!("デシリアライズされたきのこ: {:?}", shiitake);
 }
