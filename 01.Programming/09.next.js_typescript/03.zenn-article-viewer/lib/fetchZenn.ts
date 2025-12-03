e1cd618808ad1b76fcb7ecbe51e07f11866f4617
tj-10 <1010junpei@gmail.com>
2025-12-03 10:20:04 +0900

    auto: 2025-12-03 10:20:04

--- 01.Programming/09.next.js_typescript/03.zenn-article-viewer/lib/fetchZenn.ts
+++ 01.Programming/09.next.js_typescript/03.zenn-article-viewer/lib/fetchZenn.ts
@@ -4,7 +4,7 @@ import { Article } from '../types/article';
 const parser = new Parser();
 
 export async function fetchZennarticles(): Promise<Article[]> {
-  const feed = await parser.parseURL('https://zenn.dev/<ユーザー名>/feed');
+  const feed = await parser.parseURL('https://zenn.dev/juujun/feed');
   return feed.items.map(item => ({
     title: item.title || '',
     link: item.link || '',
