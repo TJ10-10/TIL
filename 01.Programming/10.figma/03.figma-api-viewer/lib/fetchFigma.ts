7f45646ebafc4d4f1eb1c38296eea549ae7eb6f4
tj-10 <1010junpei@gmail.com>
2025-12-03 11:00:17 +0900

    auto: 2025-12-03 11:00:15

--- 01.Programming/10.figma/03.figma-api-viewer/lib/fetchFigma.ts
+++ 01.Programming/10.figma/03.figma-api-viewer/lib/fetchFigma.ts
@@ -1,1 +1,18 @@
+import { FigmaDocument } from '../types/figma';
 
+export async function fetchFigmaDocument(): Promise<FigmaDocument> {
+  const fileKey = process.env.FIGMA_FILE_KEY;
+  const token = process.env.FIGMA_ACCESS_TOKEN;
+
+  const res = await fetch(`https://api.figma.com/v1/files/${fileKey}`, {
+    headers: {
+    'X-Figma-Token': token || '',
+  },
+  });
+
+  if (!res.ok) {
+    throw new Error('Figma API fetch failed');
+  }
+
+  return res.json();
+}
