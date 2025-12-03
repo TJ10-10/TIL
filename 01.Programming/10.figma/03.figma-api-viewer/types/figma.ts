67f07893d45859fcad501d9f36ce82d1ca989c4f
tj-10 <1010junpei@gmail.com>
2025-12-03 10:50:09 +0900

    auto: 2025-12-03 10:50:09

--- 01.Programming/10.figma/03.figma-api-viewer/types/figma.ts
+++ 01.Programming/10.figma/03.figma-api-viewer/types/figma.ts
@@ -1,1 +1,12 @@
+export type FigmaNode = {
+  id: string;
+  name: string;
+  type: string;
+  children?: FigmaNode[];
+};
 
+export type FigmaDocument = {
+  document: {
+    children: FigmaNode[];
+  };
+};
